8f5fec6b92d72f67195331ad747d9fe0795c058d
A5TUT0 <noahlezama2000@gmail.com>
2025-03-07 20:10:16 +0100

    Refactor utility functions to remove exports; improve code readability and maintainability

--- client/utils.js
+++ client/utils.js
@@ -1,26 +1,24 @@
-export const generateRandomUser = async () => {
+const generateRandomUser = async () => {
   const storedUser = localStorage.getItem("user");
   if (storedUser) return JSON.parse(storedUser);
-
   const response = await fetch("https://randomuser.me/api/");
   const result = await response.json();
   const newUser = {
     id: result.results[0].login.uuid,
     name: result.results[0].name.first,
   };
-
   localStorage.setItem("user", JSON.stringify(newUser));
   return newUser;
 };
 
-export const generateBackendUrl = () => {
+const generateBackendUrl = () => {
   const { protocol, host } = window.location;
   if (protocol === "http:") return `ws://${host}`;
   if (protocol === "https:") return `wss://${host}`;
   throw new Error("Unknown protocol");
 };
 
-export const generateMessage = (message, myUser) => {
+const generateMessage = (message, myUser) => {
   const messageElement = document.createElement("div");
   messageElement.classList.add(
     "flex",
@@ -31,11 +29,10 @@ export const generateMessage = (message, myUser) => {
     "opacity-0",
     "duration-300"
   );
-
   const isMyMessage = message.user.id === myUser.id;
   let messageClasses = "max-w-xs mx-2 px-4 py-3 rounded-lg rounded-br-none";
   let pictureClasses = "w-6 h-6 rounded-full mx-2 absolute top-1";
-
+  console.log(isMyMessage);
   if (isMyMessage) {
     messageClasses += " bg-blue-500 text-white";
     pictureClasses += " right-1";
@@ -45,12 +42,11 @@ export const generateMessage = (message, myUser) => {
     pictureClasses += " left-1";
     messageElement.classList.add("justify-start");
   }
-
   messageElement.innerHTML = `
       <div class="${messageClasses}">
         <p class="text-sm leading-snug">${message.user.name}: ${message.message}</p>
       </div>
-      <img src="https://i.pravatar.cc/40?u=${message.user.id}" alt="Profile" class="${pictureClasses}" />
+      <img src="https://i.pravatar.cc/40?u=${message.userId}" alt="Profile" class="${pictureClasses}" />
     `;
   return messageElement;
 };
