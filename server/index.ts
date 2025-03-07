b04996210d6ca0b6191333f79b20300be74ec1cd
Noah <noahlezama2000@gmail.com>
2025-03-07 19:10:51 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,24 @@
-import express from "express";
-import http from "http";
-import livereload from "livereload";
-import connectLiveReload from "connect-livereload";
-import { Request, Response } from "express";
-import { initializeWebsocketServer } from "./websocketserver";
-import { WebSocket } from "ws";
+import express from 'express';
+import http from 'http';
+import livereload from 'livereload';
+import connectLiveReload from 'connect-livereload';
+import { Request, Response } from 'express';
+import { initializeWebsocketServer } from './websocketserver';
+import { WebSocket } from 'ws';
 
 // Create the express server
 const app = express();
 const server = http.createServer(app);
 
+const myVar = 1;
+
 // create a livereload server
-const env = process.env.NODE_ENV || "development";
-if (env !== "production" && env !== "test") {
+const env = process.env.NODE_ENV || 'development';
+if (env !== 'production' && env !== 'test') {
   const liveReloadServer = livereload.createServer();
-  liveReloadServer.server.once("connection", () => {
+  liveReloadServer.server.once('connection', () => {
     setTimeout(() => {
-      liveReloadServer.refresh("/");
+      liveReloadServer.refresh('/');
     }, 100);
   });
   // use livereload middleware
@@ -24,10 +26,10 @@ if (env !== "production" && env !== "test") {
 }
 
 // deliver static files from the client folder like css, js, images
-app.use(express.static("client"));
+app.use(express.static('client'));
 // route for the homepage
-app.get("/", (req: Request, res: Response) => {
-  res.sendFile(__dirname + "/client/index.html");
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/client/index.html');
 });
 // Initialize the websocket server
 initializeWebsocketServer(server);
@@ -35,26 +37,21 @@ initializeWebsocketServer(server);
 //start the web server
 const startServer = (serverPort: number) => {
   server.listen(serverPort, () => {
-    console.log(
-      `Express Server started on port ${serverPort} as '${env}' Environment`
-    );
+    console.log(`Express Server started on port ${serverPort} as '${env}' Environment`);
   });
   return server;
 };
 
-if (env !== "test") {
-  const serverPort = parseInt(process.env.PORT || "3000");
+if (env !== 'test') {
+  const serverPort = parseInt(process.env.PORT || '3000');
   startServer(serverPort);
 }
 
-const waitForSocketState = (
-  socket: WebSocket,
-  state: WebSocket["readyState"]
-): Promise<void> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
+const waitForSocketState = (socket: WebSocket, state: any) => {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       if (socket.readyState === state) {
-        resolve();
+        resolve(undefined);
       } else {
         waitForSocketState(socket, state).then(resolve);
       }
